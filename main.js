1c38d208c865f1a6d5207954727060240db56ffc
uzziahram <uzziahgenella@gmail.com>
2025-07-03 17:58:20 +0800

    Update 2
    Gi separate ang members sa main js file

--- main.js
+++ main.js
@@ -1,44 +1,8 @@
 
 
-let members = [
-    owen = {
-        id: "00001",
-        name: "Owen S. Estrera \"Nataho\"",
-        profile_picture: "./members/estrera.jpg",
-        projects: [
-            {
-                name: "programees",
-                description: "A platform for programmers to collaborate and share knowledge.",
-            }
-        ]
-    },
+import members from "./members.js";
 
-    cornelius = {
-        id: "00002",
-        name: "Cornelius Timosa II \"Coke addict\"",
-        profile_picture: "./members/timosa.jpg",
-        projects: [
-            {
-                name: "projectX",
-                description: "A groundbreaking project that changes everything.",
-            }
-        ]
-    },
-
-    denise = {
-        id: "00003",
-        name: "Adrianne Caballero  \"DEN\"",
-        profile_picture: "./members/dennise.jpg",
-        projects: [
-            {
-                name: "projectY",
-                description: "An innovative project that pushes boundaries.",
-            }
-        ]
-    }
-]
-
-const show_projects = (member) => {
+const show_projects = (member_ID) => {
     // alert(`Projects for ${member.name}: ${member.projects.map(p => p.name).join(", ")}`);
     let content = document.getElementById("content");
     content.innerHTML = "";
---
+++ members.js
@@ -0,0 +1,37 @@
+let members = [
+    owen = {
+        id: "00001",
+        name: "Owen S. Estrera \"Nataho\"",
+        profile_picture: "./members/estrera.jpg",
+        projects: [
+            {
+                name: "programees",
+                description: "A platform for programmers to collaborate and share knowledge.",
+            }
+        ]
+    },
+
+    cornelius = {
+        id: "00002",
+        name: "Cornelius Timosa II \"Coke addict\"",
+        profile_picture: "./members/timosa.jpg",
+        projects: [
+            {
+                name: "projectX",
+                description: "A groundbreaking project that changes everything.",
+            }
+        ]
+    },
+
+    denise = {
+        id: "00003",
+        name: "Adrianne Caballero  \"DEN\"",
+        profile_picture: "./members/dennise.jpg",
+        projects: [
+            {
+                name: "projectY",
+                description: "An innovative project that pushes boundaries.",
+            }
+        ]
+    }
+]
